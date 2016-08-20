8c8e8e87a7584c7140a8765479dd1ff106fe334a
venkateshj99 <venkatesh19jayaraman@gmail.com>
2016-08-20 10:10:20 +0530

    Descriptions
    MainActivity - only to set layout view

--- src/com/example/phonegap/MainActivity.java
+++ src/com/example/phonegap/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		//  Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		item = itemId;
 		return true; 
 	}
 
